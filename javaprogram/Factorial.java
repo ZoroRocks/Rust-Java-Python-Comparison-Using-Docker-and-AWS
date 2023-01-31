1eb42c7d848206401110631a679c6617b6a669dc
Jaymin Parmar <89248605+ZoroRocks@users.noreply.github.com>
2023-01-31 08:52:28 +0000

    Added Java and Python code and docker configurations

---
+++ javaprogram/Factorial.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+
+public class Factorial {
+    public static void main(String[] args) {
+        long tInit = System.nanoTime();
+        int num = 30000;
+        BigInteger factorial = BigInteger.valueOf(1);
+        for (int i = 1; i <= num; i++) {
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        }
+        System.out.println("Java : " + (System.nanoTime() - tInit));
+    }
+}
